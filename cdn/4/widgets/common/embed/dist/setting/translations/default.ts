a208d760e014fc0c8e0cea5378737af2537d347f
Sven Jensen <sven@analysis.net>
2025-11-02 21:55:09 -0800

    customize Cloud Widget for demo

---
+++ cdn/4/widgets/common/embed/dist/setting/translations/default.ts
@@ -0,0 +1,17 @@
+export default {
+  _widgetLabel: 'Embed',
+  websiteAddress: 'Website address',
+  code: 'Code',
+  codePlaceholder: 'Paste the HTML code you want to embed.',
+  embedBy: 'Embed by',
+  attributeHint: 'URL from an attribute',
+  expressionHint: 'URL by an expression',
+  autoRefresh: 'Auto refresh',
+  autoInterval: 'Interval',
+  autoUnit: 'Minute',
+  reloadSetting: 'Do not reload iframe when URL changes',
+  maxLimit: 'Maximum character limit exceeded.',
+  moreDetails: 'Check more details on the supported HTML formats {detailsFormatLinkTag}here{detailsFormatLinkTag}.',
+  honorThemeFont: 'Honor theme font',
+  honorThemeFontTips: 'This option only applies to HTML text with no src nor font-family style configured.'
+}
